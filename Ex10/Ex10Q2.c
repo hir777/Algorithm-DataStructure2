fe1d0574ece1fc17f4e05e1e31b934e76f6bbe64
hir777 <69237236+hir777@users.noreply.github.com>
2021-07-15 14:00:32 +0900

    Update Ex10Q2.c

--- Ex10/Ex10Q2.c
+++ Ex10/Ex10Q2.c
@@ -10,6 +10,78 @@ id     : s1270244
 #define TRUE 1
 #define FALSE 0
 
+
+// Data structure to represent a stack
+struct stack
+{
+    int maxsize;    // define max capacity of the stack
+    int top;
+    int *items;
+};
+
+ 
+// Utility function to initialize the stack
+struct stack* newStack(int capacity)
+{
+    struct stack *pt = (struct stack*)malloc(sizeof(struct stack));
+ 
+    pt->maxsize = capacity;
+    pt->top = -1;
+    pt->items = (int*)malloc(sizeof(int) * capacity);
+ 
+    return pt;
+}
+
+ 
+// Utility function to return the size of the stack
+int size(struct stack *pt) {
+    return pt->top + 1;
+}
+
+ 
+// Utility function to check if the stack is empty or not
+int isEmpty(struct stack *pt) {
+    return pt->top == -1;                   // or return size(pt) == 0;
+}
+
+ 
+// Utility function to check if the stack is full or not
+int isFull(struct stack *pt) {
+    return pt->top == pt->maxsize - 1;      // or return size(pt) == pt->maxsize;
+}
+ 
+
+// Utility function to add an element `x` to the stack
+void push(struct stack *pt, int x)
+{
+    // check if the stack is already full. Then inserting an element would
+    // lead to stack overflow
+    if (isFull(pt))
+    {
+        printf("Overflow\nProgram Terminated\n");
+        exit(EXIT_FAILURE);
+    }
+  
+    // add an element and increment the top's index
+    pt->items[++pt->top] = x;
+}
+ 
+
+// Utility function to pop a top element from the stack
+int pop(struct stack *pt)
+{
+    // check for stack underflow
+    if (isEmpty(pt))
+    {
+        printf("Underflow\nProgram Terminated\n");
+        exit(EXIT_FAILURE);
+    }
+  
+    // decrement stack size by 1 and (optionally) return the popped element
+    return pt->items[pt->top--];
+}
+
+
 int main(int args, char *argv[]) {
     int **dp, n, b, i, j, k, *a;
     FILE *fp;
@@ -38,16 +110,23 @@ int main(int args, char *argv[]) {
         }
     }
 
+    struct stack *_stack = newStack(n+1);
+
     if( dp[n][b] ) {
         printf("YES\n");
 
         for( i = n, j = b; i >= 1; i-- ) {
             if( dp[i][j] == TRUE && dp[i-1][ j ] == FALSE ) {
-                printf("%d ", a[i-1]);
+                //printf("%d ", a[i-1]);
+                push(_stack, a[i-1]);
                 j -= a[i-1];
             }
         }
+        
+        for( i = size(_stack); i > 0; i-- )
+            printf("%d ", pop(_stack));
         printf("\n");
+
     }
     else
         printf("NO\n");
