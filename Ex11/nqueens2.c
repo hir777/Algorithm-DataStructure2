fb230d7dec1ba4d5751580abb67843eae7c64ee7
hir777 <69237236+hir777@users.noreply.github.com>
2021-07-18 17:40:05 +0900

    Create nqueens2.c

---
+++ Ex11/nqueens2.c
@@ -0,0 +1,91 @@
+// Author Hiroshi Tasaki
+// ID     s1270244
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+void queen(int, int, int, int);
+void print(int);
+int place(int, int);
+
+int board[20],count;
+ 
+int main() {
+  int n,i,j, id;
+  
+  printf(" - N Queens Problem Using Backtracking -");
+  printf("\n\nEnter number of Queens:");
+  scanf("%d", &n);
+
+  printf("\nEnter your student ID! \n Example 1224024\n->");
+  scanf("%d", &id);
+
+  queen(1, n, ((id % 100) / 10 + 1) , (id % 10) + 1);
+  printf("HoleRow: %d, HoleColumn: %d\n", ((id % 100) / 10) , (id % 10));
+  printf("The number of possible patterns to put the queens is %d.\n", count);
+
+  return 0;
+}
+ 
+
+//function for printing the solution
+void print(int n) {
+  int i,j;
+  printf("\n\nSolution %d:\n\n",++count);
+ 
+  for( i = 1;i <= n; i++ )  
+    printf("\t%d",i);
+ 
+  for( i = 1; i <= n; i++ ) {
+    printf("\n\n%d",i);
+    
+    for( j = 1; j <= n; j++ ) {
+      if(board[i]==j)
+        printf("\tQ"); //queen at i,j position
+      else
+        printf("\t-"); //empty slot
+    }
+  }
+}
+ 
+ 
+/*funtion to check conflicts
+If no conflict for desired postion returns 1 otherwise returns 0*/
+int place(int row, int column) {
+  int i;
+  
+  for( i = 1; i <= row-1; i++ ) {
+    
+    //checking column and digonal conflicts
+    if( board[i] == column )
+      return 0;
+    else if( abs(board[i] - column) == abs(i - row) )
+      return 0;
+  }
+  
+  return 1; //no conflicts
+}
+ 
+
+//function to check for proper positioning of queen
+void queen(int row, int n, int holeRow, int holeCol) {
+  int column;
+  
+  for( column = 1; column <= n; column++ ) {
+
+    if( row == holeRow && column == holeCol )
+      continue;
+
+    if( place(row, column) ) {
+      board[row]=column;    //no conflicts so place queen
+      
+      if( row == n )            //dead end
+        count++;
+        //print(n);           //printing the board configuration
+      else                  //try queen with next position
+        queen(row+1, n, holeRow, holeCol);
+    }
+
+  }
+}
